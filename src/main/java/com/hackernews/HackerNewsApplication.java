f03871dadeb36d635a18b00178f24d6612cb702c
sanjay <sanjayvrm57@gmail.com>
2021-04-03 01:09:02 +0530

    Structure

--- src/main/java/com/hackernews/HackerNewsApplication.java
+++ src/main/java/com/hackernews/HackerNewsApplication.java
@@ -1,13 +1,13 @@
-package com.hacker.news;
+package com.hackernews;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NewsApplication {
+public class HackerNewsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NewsApplication.class, args);
+		SpringApplication.run(HackerNewsApplication.class, args);
 	}
 
 }
--- src/test/java/com/hackernews/HackerNewsApplicationTests.java
+++ src/test/java/com/hackernews/HackerNewsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hacker.news;
+package com.hackernews;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NewsApplicationTests {
+class HackerNewsApplicationTests {
 
 	@Test
 	void contextLoads() {
